1e7b3f7e787ee617ee4dd4a73e7306ceabc743ba
Ryan Froehling <Ryan.J.Froehling@vanderbilt.edu>
2017-04-23 20:56:11 -0400

    Renamed to CryptoCow

--- cryptocow/settings.py
+++ cryptocow/settings.py
@@ -120,5 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
